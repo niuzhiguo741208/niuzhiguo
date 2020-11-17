a96f648cfda5d69ceb45d9d2ea8a84966b5bd791
niuzhiguo741208 <niuzhiguo@bankofyk.com>
2020-11-17 09:29:23 +0800

    This is first test

---
+++ src/com/sikiedu/test/Demo.java
@@ -0,0 +1,18 @@
+package com.sikiedu.test;/*
+ *@author lenovo
+ *@date 2020/11/16 11:07:32
+ *@description
+ */
+
+public class Demo {
+    public static void main(String[] args) {
+        int a=0;
+        for (int i=0;i<10;i++){
+            a++;
+        }
+        test();
+    }
+    private static void test(){
+        System.out.println("exec test Method!");
+    }
+}
